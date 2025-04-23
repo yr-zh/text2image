c52357fc8b9bbc4d0765adeda13ab1c262c65497
4paradigm <4paradigm@4paradigmdeMacBook-Pro.local>
2025-04-23 19:22:21 +0800

    first commit

--- .ipynb_checkpoints/submit-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-curl --location --request POST 'http://contest.4pd.io:8080/submit' \
---header 'Authorization: Bearer 37eb7e2a16ba5ac5ba77bc8c89bb94b9' \
---form-string 'benchmark=text2img' \
---form-string 'contributors=zhuangqinyu,lvjinglan,chengxi' \
---form-string 'description=basemodel:qwen2_7-sd3' \
---form-string 'product_avaliable=1' \
---form-string 'source_code=https://gitlab.4pd.io/zhuangqinyu' \
---form 'config_file=@"/mnt/data/zhuangqinyu/repos/text2image/config.yaml"'
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# 请先阅读LICENSE.mdgit remote add origin https://github.com/yr-zh/text2image.git
+
+项目介绍：
+1. 在linux环境中使用comfyUI
+2. 资源限制8GB显存下，使用gguf量化后的文生图模型，生成图像
+3. 支持多语言，调用大模型接口进行多语言的翻译
+4. 支持flux-schnell-gguf，下载地址：https://huggingface.co/city96/FLUX.1-schnell-gguf/tree/main
+5. 使用flask搭建文生图服务，并开放接口调用
